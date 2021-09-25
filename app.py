581efe992d8212b1a778cd773358faf3ea7639af
CarlosHurand <carlos_hurand@hotmail.com>
2021-09-24 21:48:36 -0500

    Final commit for today

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-from logging import debug
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
+
+con = MongoClient('localhost', 27017)
+db = con.Escuela
+cuentas = db.alumno
 
 app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return "Juan Carlos 😊"
+    
+    cursor = cuentas.find()
+    user = []
+    for doc in cursor:
+        user.append(doc)
 
+    return render_template('/users.html', data = user)
+
+@app.route('/insert')
+def insert():
+    
+    user = {"matricula": "34", "nombre": "Julion", "correo": "julion@itesm.mx", "contrasena": "2314" }
+    
+    try:
+        cuentas.insert_one(user)
+        return "<a href = '/'> Ver usuarios </a>"
+    
+    except Exception as e:
+        return "%s" % e
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    
+    if request.method == 'POST':
+        
+        password = request.form["password"]
+    
+        return "Tu password es: %s" % password
+    else:
+        return "No jala"
+
+@app.route('/test<data>')
+def test(data):
+    return "Hola %s!" % data
 
 if __name__ == "__main__":
     app.run(debug = True)
